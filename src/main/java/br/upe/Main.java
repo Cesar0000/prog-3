c201265924486f2d95465d1839bc15a1a4ae4185
italanleal <italanleal@gmail.com>
2024-07-06 14:04:40 -0300

    aCommit

---
+++ src/main/java/br/upe/Main.java
@@ -0,0 +1,16 @@
+package br.upe;
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main(String[] args){
+
+        Evento even1 = new Evento();
+
+        even1.setName("SUPER");
+
+        System.out.println(even1.getName());
+    }
+}
