bc421c679a34d718d646642144a9b342cd07d0be
italanleal <italanleal@gmail.com>
2024-07-07 12:55:48 -0300

    aCommit

--- core/src/main/java/br/upe/pojos/Keeper.java
+++ core/src/main/java/br/upe/pojos/Keeper.java
@@ -1,7 +1,9 @@
 package br.upe.pojos;
 
 public class Keeper implements KeeperInterface {
-    public GreatEvent createGreatEvent(){ return new GreatEvent(); }
+    public GreatEvent createGreatEvent(){
+        return new GreatEvent();
+    }
     public SubEvent createSubEvent(){
         return new SubEvent();
     }
