32f98da39152834d5a195b048859ec5169da01c7
Winiciuscx <carloswinicius055@gmail.com>
2024-07-25 01:26:12 -0300

    SubscriptionCRUDTest.java e loucurinha com delete

--- persistencia/src/main/java/br/upe/operations/BaseCRUD.java
+++ persistencia/src/main/java/br/upe/operations/BaseCRUD.java
@@ -9,4 +9,20 @@ public class BaseCRUD {
             while(!d.exists()) d.mkdirs();
         } catch (Exception e) {}
     }
+    //metodo para deletar o diretorio state
+//    public void deleteStateDirectory() {
+//        File directory = new File(".\\state");
+//        deleteRecursively(directory);
+//    }
+//
+//    //metodo para deletar os subarquivos
+//    private void deleteRecursively(File file) {
+//        if (file.isDirectory()) {
+//            for (File subFile : file.listFiles()) {
+//                deleteRecursively(subFile);
+//            }
+//        }
+//        file.delete();
+//    }
 }
+
